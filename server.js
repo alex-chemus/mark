a34e10b256aea3ddcb83a5445216026e1abaaed3
Alex Chemus <aachemus@gmail.com>
2022-08-31 10:14:07 +0500

    added procfile

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 80
 app.use(history())
 app.use(express.static('dist'))
 
+/*app.get('/*', (_, res) => {
+  res.sendFile('./dist/index.html')
+})*/
+
 app.listen(PORT, () => {
   console.log(`Server has been start on port ${PORT}`)
 })
