38082ca8f8a557038b6ec22bc73bcb211b5dbaa5
Alex Chemus <aachemus@gmail.com>
2022-09-03 20:36:43 +0500

    update MobilePopup

--- src/app/MobilePopup/MobilePopup.vue
+++ src/app/MobilePopup/MobilePopup.vue
@@ -164,11 +164,13 @@ li.selected {
 }
 
 .logout-btn {
-  //position: absolute;
-  //bottom: 0;
-  //left: 0;
-  margin-top: auto;
-  width: 100%;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  //margin-top: auto;
+  //width: 100%;
+  width: 70vw;
+  min-width: 250px;
   border: none;
   outline: none;
   background-color: var(--text-color-2);
